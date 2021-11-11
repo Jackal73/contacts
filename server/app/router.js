e180714235d58164e40ed9259616fe591b0c1001
Shawn Kebel <fxrender@outlook.com>
2021-11-11 00:25:04 -0600

    Say hello from API router!'

--- server/app/router.js
+++ server/app/router.js
@@ -1,7 +1,14 @@
 import Router from "express";
+import client from "./db/connections/client.js";
+import config from "./config.js";
+
+const collection = client.db(config.db.name).collection(config.db.collection);
 
 const router = new Router();
 
-// TODO: Add routes here (maybe 🤔 start with a GET test route)
+// localhost:3000/api
+router.get("/", (_, res) => {
+  res.send("Hello from API router!");
+});
 
 export default router;
