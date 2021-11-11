d72c11e17a3be515aa73fbf87e2dd7eb3d86f02e
Shawn Kebel <fxrender@outlook.com>
2021-11-10 23:49:12 -0600

    Remove the default DB_CLIENT_URL

--- server/app/config.js
+++ server/app/config.js
@@ -1,13 +1,12 @@
-// Provides config for the app in conjunction with dotenv
-// Config is the only place accesses 'dotenv'
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export default {
+  db: {
+    clientURL: process.env.DB_CLIENT_URL,
+    collection: "contacts",
+    name: "contacts",
+  },
   port: process.env.PORT || 3000,
-  db:
-    process.env.DB_CLIENT_URL ||
-    // Use localhost if no DB_CLIENT_URL is provided
-    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
 };
